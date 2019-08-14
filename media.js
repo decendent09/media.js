49e9e27b2fc336180ccdbb861ed9dae2c4d402e8
decendent09 <53884423+decendent09@users.noreply.github.com>
2019-08-14 10:19:55 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,16 @@
+let nota1 = 10
+let nota2 = 7
+ media = (nota1 + nota2)/2;
+
+ if (media >="7"){
+ console.log("Aprovado")
+
+ }else (media < "7")
+
+ console.log("Reprovado");
+
+ if (media = "10")
+ console.log("Aprovado com destinção")
+ 
+
+
